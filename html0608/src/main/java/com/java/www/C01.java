8068a204bdd8fc32070c80333413e476c0885242
KMH <KMH@DESKTOP-MBD3LU5>
2023-08-31 15:46:04 +0900

    2023년 06월 08일 html

---
+++ html0608/src/main/java/com/java/www/C01.java
@@ -0,0 +1,20 @@
+package com.java.www;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/C01")
+public class C01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
